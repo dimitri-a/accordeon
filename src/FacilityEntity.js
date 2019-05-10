921ca1b732da89a6974076bc039b8cbbc26c6d7e
Dimitri C <couwenberg@gmail.com>
2019-05-10 11:42:45 +1000

    clean

--- src/FacilityEntity.js
+++ src/FacilityEntity.js
@@ -1,21 +1,15 @@
-import React, { Fragment , useState } from "react";
+import React, { Fragment, useState } from "react";
 import FacilityEntityDetail from "./FacilityEntityDetail";
 
 const FacilityEntity = ({ entity }) => {
-  
-
-  const [detailsVisible,setDetailsVisible] = useState(false)
-  console.log(detailsVisible);
+  const [detailsVisible, setDetailsVisible] = useState(false);
 
   return (
     <div onClick={() => setDetailsVisible(!detailsVisible)}>
-
-    {/* {detailsVisible && (<div>testing detailsVisible is true!!!!</div>)} */}
-    
+      {/* {detailsVisible && (<div>testing detailsVisible is true!!!!</div>)} */}
       {entity.name}, {entity.id},Facility limit: {entity.limit},
-      {detailsVisible && (entity.facilities.map(f => (
-        <FacilityEntityDetail detail={f} />
-      )))}
+      {detailsVisible &&
+        entity.facilities.map(f => <FacilityEntityDetail detail={f} />)}
     </div>
   );
 };
