8660502b906a52522fbb3154ee48aee3cee63929
Dimitri C <couwenberg@gmail.com>
2019-05-13 13:17:34 +1000

    sort by borrowername

--- src/FacilitiesSummary.js
+++ src/FacilitiesSummary.js
@@ -8,9 +8,18 @@ export default class FacilitiesSummary extends Component {
     console.log('data',data)
   }
 
-  render() {
+sortData(entities){
+  return entities.sort(function(a, b){
+    if(a.name < b.name) { return -1; }
+    if(a.name > b.name) { return 1; }
+    return 0;
+})
+}
+  
 
-    let results = data.entities;
+  render() {
+    let results = this.sortData(data.entities);
+    debugger
     console.log(results)
     return (
       <div>
--- src/facilities.js
+++ src/facilities.js
@@ -1,38 +1,53 @@
-const data  ={
-  "entities": [
+const data = {
+  entities: [
     {
-      "name": "Broomsticks PTY",
-      "id":34098365,
-      "limit":550000,
-      "facilities":[
-          {
-          "type":"Home loan",
-          "account":"032654 987465",
-          "existing":true,
-          "modified":"04/12/2018"
-          },
-          {
-            "type":"Credt card",
-            "account":"032654 987465",
-            "existing":false,
-            "modified":"04/12/2018"
-            }
+      name: "zBroomsticks PTY",
+      id: 34098365,
+      limit: 550000,
+      facilities: [
+        {
+          type: "Home loan",
+          account: "032654 987465",
+          existing: true,
+          modified: "04/12/2018",
+          entityType: "personal"
+        },
+        {
+          type: "Credt card",
+          account: "032654 987465",
+          existing: false,
+          modified: "04/12/2018",
+          entityType: "business"
+        },
+        {
+          type: "Credt card",
+          account: "032654 987465",
+          existing: false,
+          modified: "04/12/2018",
+          entityType: "mixed"
+        },
+        {
+          type: "Credt card",
+          account: "032654 987465",
+          existing: false,
+          modified: "04/12/2018",
+          entityType: "outside"
+        }
       ]
     },
     {
-        "name": "Mr John Doe -3409865, Mrs Jane Doe -34098365",
-        "id":34098365,
-        "limit":550000,
-        "facilities":[
-            {
-            "type":"Overdraft",
-            "account":"032654 987465",
-            "existing":false,
-            "modified":"04/12/2018"
-            }
-            
-        ]
-      }
+      name: "Mr John Doe -3409865, Mrs Jane Doe -34098365",
+      id: 34098365,
+      limit: 550000,
+      facilities: [
+        {
+          type: "Overdraft",
+          account: "032654 987465",
+          existing: false,
+          modified: "04/12/2018"
+        }
+      ]
+    }
   ]
-}
+};
 export default data;
